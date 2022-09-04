3f687a1f2afbee1692cf7731b9ea729b5c7908f2
Amit Kumar Dubey <tech.admin@ardouranalytics.com>
2022-09-04 11:18:32 +0530

    secure link

--- App.tsx
+++ App.tsx
@@ -43,31 +43,10 @@ export default function App() {
   const navigationRef = useNavigationContainerRef();
   useReduxDevToolsExtension(navigationRef);
 
-  // let [fontsLoaded] = useFonts({
-  //   Poppins_100Thin,
-  //   Poppins_100Thin_Italic,
-  //   Poppins_200ExtraLight,
-  //   Poppins_200ExtraLight_Italic,
-  //   Poppins_300Light,
-  //   Poppins_300Light_Italic,
-  //   Poppins_400Regular,
-  //   Poppins_400Regular_Italic,
-  //   Poppins_500Medium,
-  //   Poppins_500Medium_Italic,
-  //   Poppins_600SemiBold,
-  //   Poppins_600SemiBold_Italic,
-  //   Poppins_700Bold,
-  //   Poppins_700Bold_Italic,
-  //   Poppins_800ExtraBold,
-  //   Poppins_800ExtraBold_Italic,
-  //   Poppins_900Black,
-  //   Poppins_900Black_Italic,
-  // });
-
   let persistor = persistStore(store);
 
   const linking = {
-    prefixes: [prefix, "http://thehealthycomparison.com"],
+    prefixes: [prefix, "https://thehealthycomparison.com"],
     config: {
       screens: {
         "Link Device": "link/:code",
