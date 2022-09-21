1d514b2b12d7ef5fdee299c84f28c24a511966b1
Amit Kumar Dubey <tech.admin@ardouranalytics.com>
2022-09-21 08:55:14 +0530

    remove extra screens

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -5,7 +5,7 @@ export const config = Object.freeze({
         // for emulator
         // BASE_URL: "http://10.0.2.2:8000/api/v1",
         // for tunnel
-        BASE_URL: "https://06d8-122-182-221-161.in.ngrok.io/api/v1",
+        BASE_URL: "https://f7fe-122-182-221-161.in.ngrok.io/api/v1",
     },
     "google fit": {
         issuer: "https://accounts.google.com",
